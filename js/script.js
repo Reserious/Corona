b7a3d90cc2c36c6c4a6479400d4d4b1aae3f5302
Khambat <hamboenzo@gmail.com>
2020-03-25 22:49:58 +0200

    Add api

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(function ($) {
+    $.getJSON( "https://coronavirus-19-api.herokuapp.com/all", function( data ) {
+        var items = [];
+        $.each( data, function( key, val ) {
+            items.push( "" +
+                "<div class='live__" + key + "'>" +
+                "   <div class='col__title'>" + val + "</div>" +
+                    "<div class='col__suptitle'>Total " + key + "</div>" +
+                "</div>" +
+                "" );
+        });
+
+        $(items.join( "" )).appendTo( ".live__update" );
+    });
+
+    $.getJSON( "https://coronavirus-19-api.herokuapp.com/countries", function( data ) {
+        var items = [];
+        $.each( data, function( key, val ) {
+            items.push( "" +
+                "<tr>" +
+                "   <td>" + val.country + "</td>" +
+                "   <td>" + val.cases + "</td>" +
+                "   <td>" + val.todayCases + "</td>" +
+                "   <td>" + val.deaths + "</td>" +
+                "   <td>" + val.todayDeaths + "</td>" +
+                "   <td>" + val.critical + "</td>" +
+                "   <td>" + val.recovered + "</td>" +
+                "</tr>" +
+                "" );
+        });
+
+        $(items.join( "" )).appendTo( ".table__container" );
+    });
+});
